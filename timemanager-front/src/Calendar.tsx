daf9366bc9c60497fbc5c31cb109f6dc304e77d9
Tturna <42873350+Tturna@users.noreply.github.com>
2026-01-09 10:06:25 +0200

    some event-calendar TS types, basic calendar event adding logic

--- timemanager-front/src/Calendar.tsx
+++ timemanager-front/src/Calendar.tsx
@@ -1,11 +1,47 @@
 import { useEffect, useRef } from 'react';
-import { createCalendar, destroyCalendar, TimeGrid, DayGrid, List } from '@event-calendar/core';
+import { createCalendar, destroyCalendar, TimeGrid, DayGrid, List, type Event } from '@event-calendar/core';
 // Import CSS if your build tool supports it
 import '@event-calendar/core/index.css';
 
 function Calendar() {
     const calendarRef = useRef<any>(null)
     const calendarParentRef = useRef<HTMLDivElement | null>(null)
+    const eventsRef = useRef<Event[]>(
+        [
+            {
+                id: "1",
+                resourceIds: [],
+                allDay: false,
+                start: new Date(2026, 0, 5, 8, 30),
+                end: new Date(2026, 0, 5, 15),
+                title: "My Title",
+                display: "auto",
+                classNames: [],
+                styles: [],
+                extendedProps: {}
+            }
+        ]
+    )
+
+    const addEvent = () => {
+        let prev = eventsRef.current.at(-1)
+
+        if (!prev) return
+
+        let newStart = new Date(prev.start)
+        let newEnd = new Date(prev.end)
+        newStart.setDate(newStart.getDate() + 1)
+        newEnd.setDate(newEnd.getDate() + 1)
+
+        eventsRef.current.push({
+            ...prev,
+            id: (parseInt(prev.id) + 1).toString(),
+            start: newStart,
+            end: newEnd
+        })
+
+        calendarRef.current.setOption("events", eventsRef.current)
+    }
 
     useEffect(() => {
         calendarRef.current = createCalendar(
@@ -16,9 +52,7 @@ function Calendar() {
             // Options object
             {
                 view: 'timeGridWeek',
-                events: [
-                    // your list of events
-                ]
+                events: eventsRef.current
             }
         );
 
@@ -31,7 +65,12 @@ function Calendar() {
         }
     }, [])
 
-    return <div ref={calendarParentRef} id="calendar"></div>
+    return (
+        <>
+        <button onClick={addEvent}>Add</button>
+        <div ref={calendarParentRef} id="calendar"></div>
+        </>
+    )
 }
 
 export default Calendar
--- timemanager-front/src/types/event-calendar-core.d.ts
+++ timemanager-front/src/types/event-calendar-core.d.ts
@@ -1,7 +1,30 @@
 declare module '@event-calendar/core' {
-  export function createCalendar(...args: any[]): any;
-  export function destroyCalendar(...args: any[]): void;
-  export const TimeGrid: any;
-  export const DayGrid: any;
-  export const List: any;
+    export type Content =
+        | string
+        | { html: string }
+        | { domNodes: Node[] }
+
+    export interface Event {
+        id: string,
+        resourceIds: any[],
+        allDay: bool,
+        start: Date,
+        end: Date,
+        title: Content,
+        editable?: bool,
+        startEditable?: bool,
+        durationEditable?: bool,
+        display: "auto" | "background" | "ghost" | "preview" | "pointer",
+        backgroundColor?: string,
+        textColor?: string,
+        classNames: any[],
+        styles: any[],
+        extendedProps: {}
+    }
+
+    export function createCalendar(...args: any[]): any;
+    export function destroyCalendar(...args: any[]): void;
+    export const TimeGrid: any;
+    export const DayGrid: any;
+    export const List: any;
 }
