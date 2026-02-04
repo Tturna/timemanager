9859d7229c2f3fe4e669ed54fbd41da0e81a0933
Tturna <42873350+Tturna@users.noreply.github.com>
2026-02-04 14:11:00 +0200

    move calendar active view range indicator

--- front/src/calendar/useCalendar.tsx
+++ front/src/calendar/useCalendar.tsx
@@ -89,7 +89,7 @@ function useCalendar(
                 eventResize: handleEventResize,
                 eventClick: handleEventClick,
                 theme: setCalendarStyling,
-                headerToolbar: { start: "title today prev,next add export", center: "", end: ""}
+                headerToolbar: { start: "today prev,next add export title", center: "", end: ""}
             }
         );
 
